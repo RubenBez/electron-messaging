577bf9382d0d5ad95177674baa1402eb61379c2c
Ruben Bezuidenhout <ruben@bitcube.tech>
2019-11-22 09:51:05 +0200

    Added Redux, React-Redux and Redux-toolbox

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as Redux from "redux";
 import "./index.css";
 
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+function increase() {
+  return INCREASE;
+}
+
+function decrease() {
+  return DECREASE;
+}
+
+function counter(state = 0, action: any) {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
+    case DECREASE:
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = Redux.createStore(counter);
+
 ReactDOM.render(
-  <div>hello world from React! </div>,
+  <button onClick={() => store.dispatch(increase())}>Increase</button>,
   document.getElementById("root")
 );
